73db3706815fa8729c952dac4e092dfa1eacad71
Eddieee2 <lescoe1234@gmail.com>
2025-08-27 10:32:55 -0600

    primer commit

---
+++ If1/Program.cs
@@ -0,0 +1,11 @@
+﻿int edad;
+Console.Write("Introduce tu edad: ");
+edad = int.Parse(Console.ReadLine());
+if (edad >= 18)
+{
+    Console.WriteLine("Eres mayor de edad.");
+}
+else
+{
+    Console.WriteLine("Eres menor de edad.");
+}
