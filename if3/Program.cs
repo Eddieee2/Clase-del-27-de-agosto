4df83b0c2425b0f4c810fa204d9e1b96fc707249
Eddieee2 <lescoe1234@gmail.com>
2025-08-29 09:34:06 -0600

    uso de if y switch en C#

---
+++ if3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Escribe un año: ");
+        int año = int.Parse(Console.ReadLine());
+
+        if (año % 4 == 0)
+        {
+            if (año % 100 == 0)
+            {
+                if (año % 400 == 0)
+                {
+                    Console.WriteLine("El año es bisiesto.");
+                }
+                else
+                {
+                    Console.WriteLine("El año no es bisiesto.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El año es bisiesto.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("El año no es bisiesto.");
+        }
+    }
+}
